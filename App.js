c472afdbe6cb0a9347f9ac0f178afbc9505ab869
Thieres <thieresllima@gmail.com>
2020-10-26 11:10:23 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+function App() {
+  return (
+    <>
+      <StatusBar barStyle='dark-content' />
+    </>
+  );
+}
+
+export default App;
